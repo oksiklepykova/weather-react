7cf869a1e51659dfb5dd9aa4a87570544035c5cb
Пользователь <oksiklepykova@gmail.com>
2023-02-11 10:44:23 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>  
       <Weather />
+      <footer>
+        <a href="https://github.com/oksiklepykova/weather-react" target="_blank" rel="noreferrer">Open-source code</a>{""}
+        , by{" "}
+        <a href="/" target="_blank" rel="noreferrer">Oksana Klepykova</a>{" "}
+        from{" "}
+        <a href="http://shecodes.io" target="_blank" rel="noreferrer">She Codes</a>
+      </footer>
     </div>
+    
   );
 }
 
